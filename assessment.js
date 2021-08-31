54540e84e444cdef54de4d4d39befc8dda714f2f
uera-3980 <89629245+uera-3980@users.noreply.github.com>
2021-08-31 17:26:04 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -30,7 +30,7 @@ removeAllChildren(tweetDivided);
     const hrefValue = 
     'https://twitter.com/intent/tweet?button_hashtag='+
     encodeURIComponent('あなたのいいところ')+
-    '&ref_src=twsrc%5Etfw'
+    '&ref_src=twsrc%5Etfw';
     anchor.setAttribute('href',hrefValue);
     anchor.className = 'twitter-hashtag-button';
     anchor.setAttribute=('data-text',result);
